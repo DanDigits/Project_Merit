e664b440b13a25fc866cc2f864076c7f6298294e
Daniel Cruz-Castro <60457928+DanDigits@users.noreply.github.com>
2023-09-15 19:34:55 -0400

    Updated getBaseURL()

--- utils/getPath.js
+++ utils/getPath.js
@@ -1,20 +1,21 @@
 /* eslint-disable prettier/prettier */
 function getBaseURL() {
   // Check if a domain name has been assigned, and correct application URLs accordingly
-  if (!process.env.NEXT_PUBLIC_NEXTAUTH_URL) {
+  if (
+    !process.env.NEXT_PUBLIC_NEXTAUTH_URL ||
+    process.env.NEXT_PUBLIC_NEXTAUTH_URL == "localhost"
+  ) {
     return `http://localhost:3000`;
-  } else if (process.env.NEXT_PUBLIC_NEXTAUTH_URL != "localhost") {
-    if (
-      process.env.NEXT_PUBLIC_NEXTAUTH_URL.toLowerCase().includes("http://")
-    ) {
-      return `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}:80`;
-    } else if (
-      process.env.NEXT_PUBLIC_NEXTAUTH_URL.toLowerCase().includes("https://")
-    ) {
-      return `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}:443`;
-    } else {
-      return `https://${process.env.NEXT_PUBLIC_NEXTAUTH_URL}:443`;
-    }
+  } else if (
+    process.env.NEXT_PUBLIC_NEXTAUTH_URL.toLowerCase().includes("http://")
+  ) {
+    return `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}:80`;
+  } else if (
+    process.env.NEXT_PUBLIC_NEXTAUTH_URL.toLowerCase().includes("https://")
+  ) {
+    return `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}:443`;
+  } else {
+    return `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}:${process.env.NEXTAUTH_PORT}`;
   }
 }
 
