5997b127e0d1359172912bad548525d9193aff8f
Daniel Cruz-Castro <60457928+DanDigits@users.noreply.github.com>
2023-09-15 18:23:40 -0400

    Testing production

--- utils/getPath.js
+++ utils/getPath.js
@@ -1,14 +1,16 @@
 /* eslint-disable prettier/prettier */
 function getBaseURL() {
-  console.error(process.env.NEXTAUTH_URL);
+  console.error(process.env.NEXT_PUBLIC_NEXTAUTH_URL);
   // Check if a domain name has been assigned, and correct application URLs accordingly
-  if (!process.env.NEXTAUTH_URL) {
+  if (!process.env.NEXT_PUBLIC_NEXTAUTH_URL) {
     return `http://localhost:3000`;
-  } else if (process.env.NEXTAUTH_URL != "localhost") {
-    if (process.env.NEXTAUTH_URL.toLowerCase().includes("http://")) {
-      return `${process.env.NEXTAUTH_URL}:80`;
+  } else if (process.env.NEXT_PUBLIC_NEXTAUTH_URL != "localhost") {
+    if (
+      process.env.NEXT_PUBLIC_NEXTAUTH_URL.toLowerCase().includes("http://")
+    ) {
+      return `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}:80`;
     } else {
-      return `http://${process.env.NEXTAUTH_URL}:80`;
+      return `http://${process.env.NEXT_PUBLIC_NEXTAUTH_URL}:80`;
     }
   }
 }
