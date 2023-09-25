cd6e5507d744689845d33678f55c7443f26897b5
Mari Peele <mpeele@geometricprogress.com>
2023-09-24 21:03:45 -0400

    added call for getUser

--- src/app/Dashboard/Profile/page.jsx
+++ src/app/Dashboard/Profile/page.jsx
@@ -12,21 +12,48 @@ import { Box } from "@chakra-ui/react";
 import { signOut } from "next-auth/react";
 import { useEffect } from "react";
 import { getSession } from "next-auth/react";
+import { getUser } from "./../../actions/User.js";
 
 export default function Page() {
   const [email, setEmail] = useState("");
-  const [rank, setRank] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [suffix, setSuffix] = useState("");
+  //const [rank, setRank] = useState("");
+  //const [firstName, setFirstName] = useState("");
+  //const [lastName, setLastName] = useState("");
+  //const [suffix, setSuffix] = useState("");
+  const [hasEmail, setHasEmail] = useState(false);
+  const [profile, setProfile] = useState("");
+  const [hasProfile, setHasProfile] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    getSession().then((session) => setEmail(session.user.email));
-    getSession().then((session) => setRank(session.user.rank));
-    getSession().then((session) => setFirstName(session.user.firstName));
-    getSession().then((session) => setLastName(session.user.lastName));
-    getSession().then((session) => setSuffix(session.user.suffix));
-  }, []);
+    if (!hasEmail) {
+      //console.log("!hasemail");
+      setIsLoading(true);
+      setHasError(false);
+      getSession()
+        .then((session) => setEmail(session.user.email))
+        .then(() => setHasEmail(true));
+    }
+    if (hasEmail && !hasProfile) {
+      console.log("hasEmail && !hasprovile", email, hasProfile);
+      setIsLoading(true);
+      console.log("isLoading:", isLoading);
+      setHasError(false);
+      getUser({ email }).then((response) => {
+        response.ok
+          ? response
+              .json()
+              .then((response) => setProfile(response))
+              .then(setHasProfile(true))
+          : setHasError(true).then(console.log("hasError:", hasError));
+      });
+    }
+    if (hasEmail && hasProfile) {
+      console.log("hasEmail && hasProfile", email, hasProfile);
+      setIsLoading(false);
+    }
+  }, [hasEmail, hasProfile, email]);
   const handleLogout = (e) => {
     e.preventDefault();
     signOut({ callbackUrl: "/Auth/Logout" });
@@ -52,14 +79,14 @@ export default function Page() {
                 <Text fontSize="2xl" fontWeight="bold">
                   Rank
                 </Text>
-                <Text fontSize="lg">{rank}</Text>
+                <Text fontSize="lg">{profile.rank}</Text>
               </Box>
               <Box p="5">
                 <Text fontSize="2xl" fontWeight="bold">
                   Name
                 </Text>
                 <Text fontSize="lg">
-                  {firstName} {lastName} {suffix}
+                  {profile.firstName} {profile.lastName} {profile.suffix}
                 </Text>
               </Box>
             </VStack>
