57995da6f09eb1acdd77fe82ce6b2cd7292ddfae
Mari Peele <mpeele@geometricprogress.com>
2023-09-26 22:06:55 -0400

    added categories to dashboard layout

--- src/app/Dashboard/Home/page.jsx
+++ src/app/Dashboard/Home/page.jsx
@@ -1,11 +1,5 @@
 "use client";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Button,
-} from "@chakra-ui/react";
+import { Card, CardHeader, CardBody } from "@chakra-ui/react";
 import { Stack, Text, VStack, HStack, Heading } from "@chakra-ui/layout";
 import { useEffect, useState } from "react";
 import { getSession } from "next-auth/react";
@@ -31,18 +25,31 @@ export default function Page() {
         w={{ md: "lg" }}
         bgColor={"white"}
       >
-        <Stack>
+        <Stack alignItems={"center"}>
           <Heading
             fontSize={{ base: "25px", md: "30px" }}
             color="#331E38"
+            align={"center"}
             py={"5"}
           >
             Welcome {rank} {lastName} {suffix}
           </Heading>
-          ;
-          <Text fontSize={20} color="#331E38" py={"2"}>
-            You have written...
-          </Text>
+          <Card
+            bgColor={"#F4E8C1"}
+            size={{ base: "sm", md: "lg" }}
+            variant={"outline"}
+            align={"center"}
+            width={{ md: "md" }}
+          >
+            <CardHeader pb={"5"}>
+              <Heading fontSize={"20"}>Total Reports for the Year</Heading>
+            </CardHeader>
+            <CardBody pt={"0"} pb={"5"}>
+              <Text color={"#331E38"} fontSize={"30"} fontWeight={"semibold"}>
+                15
+              </Text>
+            </CardBody>
+          </Card>
           <HStack spacing={4} alignSelf={"center"}>
             <Card
               bgColor={"#A0C1B9"}
@@ -52,10 +59,12 @@ export default function Page() {
             >
               <CardBody>
                 <VStack>
-                  <Text color={"#331E38"} fontSize={30} fontWeight={"semibold"}>
-                    #
+                  <Text color={"#331E38"} fontWeight={"semibold"}>
+                    Primary / Additional Duties
                   </Text>
-                  <Text color={"#331E38"}>reports for Q#</Text>
+                  <Text color={"#331E38"} fontSize={30}>
+                    2
+                  </Text>
                 </VStack>
               </CardBody>
             </Card>
@@ -67,44 +76,52 @@ export default function Page() {
             >
               <CardBody>
                 <VStack>
-                  <Text color={"#331E38"} fontSize={30} fontWeight={"semibold"}>
-                    #
+                  <Text color={"#331E38"} fontWeight={"semibold"}>
+                    Standards, Conduct, Character & Military Bearing
                   </Text>
-                  <Text color={"#331E38"}>reports for 2023</Text>
+                  <Text color={"#331E38"} fontSize={30}>
+                    5
+                  </Text>
                 </VStack>
               </CardBody>
             </Card>
           </HStack>
-          <Text
-            fontSize={20}
-            color="#331E38"
-            pt={"8"}
-            pe={"5"}
-            alignSelf={"center"}
-          >
-            Your last report was written # days ago.
-          </Text>
-          <Card
-            bgColor={"#706993"}
-            size={{ base: "sm", md: "lg" }}
-            variant={"outline"}
-            align={"left"}
-            width={{ md: "md" }}
-          >
-            <CardHeader pb={"5"}>
-              <Heading fontSize={"20"}>2023 Q# July 1</Heading>
-            </CardHeader>
-            <CardBody pt={"0"} pb={"5"}>
-              <Text color={"#331E38"} fontSize={"15"} fontWeight={"semibold"}>
-                Preview
-              </Text>
-            </CardBody>
-            <CardFooter pt={"0"}>
-              <Button bg="white" _hover={{ bg: "#331E38", color: "white" }}>
-                View Last Report
-              </Button>
-            </CardFooter>
-          </Card>
+          <HStack spacing={4} alignSelf={"center"}>
+            <Card
+              bgColor={"#70A0AF"}
+              size={{ base: "sm", md: "lg" }}
+              variant={"outline"}
+              align={"center"}
+            >
+              <CardBody>
+                <VStack>
+                  <Text color={"#331E38"} fontWeight={"semibold"}>
+                    Training Requirements
+                  </Text>
+                  <Text color={"#331E38"} fontSize={30}>
+                    7
+                  </Text>
+                </VStack>
+              </CardBody>
+            </Card>
+            <Card
+              bgColor={"#A0C1B9"}
+              size={{ base: "sm", md: "lg" }}
+              variant={"outline"}
+              align={"center"}
+            >
+              <CardBody>
+                <VStack>
+                  <Text color={"#331E38"} fontWeight={"semibold"}>
+                    Teamwork / Followership
+                  </Text>
+                  <Text color={"#331E38"} fontSize={30}>
+                    0
+                  </Text>
+                </VStack>
+              </CardBody>
+            </Card>
+          </HStack>
         </Stack>
       </Card>
     </>
