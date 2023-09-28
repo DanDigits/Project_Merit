9b86c2590a837fca6f469d7495ec90899a58f592
Mari Peele <mpeele@geometricprogress.com>
2023-09-28 00:12:49 -0400

    removed outdated comment

--- src/app/Dashboard/NewReport/report.jsx
+++ src/app/Dashboard/NewReport/report.jsx
@@ -45,11 +45,6 @@ export default function Report(report_mode) {
 
   var state;
 
-  //temp
-  /* must get report id somehow
-  setData(getReport(reportId));
-  */
-
   if (report_mode === "View") {
     state = true;
   } else state = false;
