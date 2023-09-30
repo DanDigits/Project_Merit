422cddd3b7cbcad4f46d943b71d1f6756ff2ae29
Mari Peele <mpeele@geometricprogress.com>
2023-09-30 19:16:07 -0400

    changed dialog for password errors and success

--- src/app/Dashboard/Profile/UpdatePassword.jsx
+++ src/app/Dashboard/Profile/UpdatePassword.jsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getSession } from "next-auth/react";
 import {
   Card,
   Button,
@@ -15,13 +16,37 @@ import {
 } from "@chakra-ui/react";
 import PasswordDialog from "./PasswordDialog.jsx";
 
-//import { updateUser } from "src/app/actions/User";
+import { updatePassword } from "src/app/actions/User";
 
 export default function UpdatePassword() {
-  const [current, setCurrent] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [password2, setPassword2] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [newPassword2, setNewPassword2] = useState("");
   const [status, setStatus] = useState(false);
+
+  useEffect(() => {
+    getSession().then((session) => setEmail(session.user.email));
+    console.log("Email:", email);
+  });
+
+  const handleSubmitInfo = (e) => {
+    e.preventDefault();
+
+    if (newPassword === newPassword2) {
+      updatePassword({ email, password, newPassword }).then((response) => {
+        if (response.ok) {
+          {
+            <PasswordDialog />;
+          }
+        } else {
+          setStatus("old");
+        }
+      });
+    } else {
+      setStatus("new");
+    }
+  };
 
   return (
     <>
@@ -37,28 +62,9 @@ export default function UpdatePassword() {
         </CardHeader>
         <CardBody>
           <div>
-            <FormControl id="current">
-              <FormLabel mb={1} fontSize={15} color={"black"}>
-                Current Password
-              </FormLabel>
-              <Input
-                type="password"
-                value={current}
-                variant="login"
-                borderWidth={"2px"}
-                borderColor={"#70A0AF"}
-                bg="#EDF2F7"
-                mb={3}
-                size={"md"}
-                minLength={8}
-                maxLength={32}
-                onChange={(e) => setCurrent(e.target.value)}
-              />
-              <FormErrorMessage>Current password is required.</FormErrorMessage>
-            </FormControl>
             <FormControl id="password">
               <FormLabel mb={1} fontSize={15} color={"black"}>
-                New Password
+                Current Password
               </FormLabel>
               <Input
                 type="password"
@@ -73,15 +79,15 @@ export default function UpdatePassword() {
                 maxLength={32}
                 onChange={(e) => setPassword(e.target.value)}
               />
-              <FormErrorMessage>Please enter new password.</FormErrorMessage>
+              <FormErrorMessage>Current password is required.</FormErrorMessage>
             </FormControl>
-            <FormControl id="confirmPassword">
+            <FormControl id="newPassword">
               <FormLabel mb={1} fontSize={15} color={"black"}>
-                Confirm New Password
+                New Password
               </FormLabel>
               <Input
                 type="password"
-                value={password2}
+                value={newPassword}
                 variant="login"
                 borderWidth={"2px"}
                 borderColor={"#70A0AF"}
@@ -90,12 +96,34 @@ export default function UpdatePassword() {
                 size={"md"}
                 minLength={8}
                 maxLength={32}
-                onChange={(e) => setPassword2(e.target.value)}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+              <FormErrorMessage>Please enter new password.</FormErrorMessage>
+            </FormControl>
+            <FormControl id="newPassword2">
+              <FormLabel mb={1} fontSize={15} color={"black"}>
+                Confirm New Password
+              </FormLabel>
+              <Input
+                type="password"
+                value={newPassword2}
+                variant="login"
+                borderWidth={"2px"}
+                borderColor={"#70A0AF"}
+                bg="#EDF2F7"
+                mb={3}
+                size={"md"}
+                minLength={8}
+                maxLength={32}
+                onChange={(e) => setNewPassword2(e.target.value)}
               />
               <FormErrorMessage>
                 Password Confirmation is required.
               </FormErrorMessage>
             </FormControl>
+            {status === "new" && <p>Confirmation must match new password.</p>}
+            {status === "success" && <p>Password Updated</p>}
+            {status === "old" && <p>Current password is incorrect.</p>}
           </div>
         </CardBody>
         <CardFooter>
@@ -106,13 +134,12 @@ export default function UpdatePassword() {
               _hover={{ bgColor: "#706993", color: "white" }}
               form="report-form"
               type="submit"
-              onClick={() => setStatus(true)}
+              onClick={(e) => handleSubmitInfo(e)}
             >
               Update
             </Button>
           </ButtonGroup>
         </CardFooter>
-        {PasswordDialog(status)}
       </Card>
     </>
   );
