98ff937043dc5e2bb74e251bff0375fa42b9d1c9
Daniel Cruz-Castro <60457928+DanDigits@users.noreply.github.com>
2023-11-08 02:54:31 -0500

    Added Github Actions & Terraform/AWS pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_version = "~> 1.5.0"
+
+  backend "s3" {
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ecr" {
+  source        = "./modules/ecr"
+  ecr_repo_name = var.ecr_repo_name
+}
+
+module "ecs" {
+  source   = "./modules/ecs"
+  repo_url                       = module.ecr.repository_url
+
+  availability_zones             = var.availability_zones
+  cluster_name                   = var.cluster_name
+
+  task_family                    = var.task_family
+  container_port                 = var.container_port
+  container_name                 = var.container_name
+  task_execution_role_name       = var.task_execution_role_name
+
+  db_uri                         = var.db_uri
+  nextauth_port                  = var.nextauth_port
+  nextauth_url                   = var.nextauth_url
+  nextauth_public_url            = var.nextauth_public_url
+
+  application_load_balancer_name = var.application_load_balancer_name
+  target_group                   = var.target_group
+  service_name                   = var.service_name
+  domain_certificate             = var.domain_certificate
+}
